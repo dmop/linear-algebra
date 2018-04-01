585891932399a77a5527e46f6fc7e035e9390942
Danilo <dmop@cin.ufpe.br>
2018-04-01 20:43:37 -0300

    more functions

--- vector.py
+++ vector.py
@@ -24,10 +24,10 @@ class Vector(object):
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
-    def __eq__(self, v):
+    def __eq__(self, v):  # compare if the vectors are equal
         return self.coordinates == v.coordinates
 
-    def plus(self, v):
+    def plus(self, v):  # add the two vectors
         new_coordinates = [x+y for x,
                            y in zip(self.coordinates, v.coordinates)]
         # new_coordinates = []
@@ -36,7 +36,7 @@ class Vector(object):
         # new_coordinates.append(self.coordinates[i]+v.coordinates[i])
         return Vector(new_coordinates)
 
-    def minus(self, v):
+    def minus(self, v):  # add the two vectors
         new_coordinates = [x-y for x,
                            y in zip(self.coordinates, v.coordinates)]
         return Vector(new_coordinates)
@@ -78,12 +78,77 @@ class Vector(object):
             else:
                 raise e
 
+    def is_paralel_to(self, v):
+        return (self.is_zero() or
+                v.is_zero() or
+                self.angle_with(v) == 0 or
+                self.angle_with(v) == pi)
 
-myVector = Vector([-0.221, 7.437])
+    def is_orthogonal_to(self, v, tolerance=1e-10):
+        return abs(self.dot(v)) < tolerance
+
+    def is_zero(self, tolerance=1e-10):
+        return self.magnitude() < tolerance
+
+    def component_orthogonal_to(self, basis):
+        try:
+            projection = self.component_parallel_to(basis)
+            return self.minus(projection)
+        except Exception as e:
+            # if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+                # raise Exception(self.NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG)
+            # else:
+            raise e
+
+    def component_parallel_to(self, basis):
+        try:
+            u = basis.normalized()
+            weight = self.dot(u)
+            return u.times_scalar(weight)
+
+        except Exception as e:
+            # if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+            # raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+            # else:
+            raise e
+
+    def area_of_triangle_with(self, v):
+        return self.area_of_parallelogram_with(v)/Decimal('2.0')
+
+    def area_of_parallelogram_with(self, v):
+        cross_product = self.cross(v)
+        return cross_product.magnitude()
+
+    def cross(self, v):
+        try:
+            x_1, y_1, z_1 = self.coordinates
+            x_2, y_2, z_2 = v.coordinates
+            new_coordinates = [y_1 * z_2 - y_2 * z_1,
+                               -(x_1 * z_2 - x_2 * z_1),
+                               x_1 * y_2 - x_2 * y_1]
+            return Vector(new_coordinates)
+
+        except ValueError as e:
+            msg = str(e)
+            if msg == 'need more than 2 values to unpack':
+                self_embedded_in_R3 = Vector(self.coordinates + ('0',))
+                v_embedded_in_R3 = Vector(v.coordinates + ('0',))
+                return self_embedded_in_R3.cross(v_embedded_in_R3)
+
+            elif (msg == 'too many values to unpack' or
+                  msg == 'need more than 2 value to unpack'):
+                #   raise Exception(self.ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG)
+                raise e
+            else:
+                raise e
+
+
+myVector = Vector([3.039, 1.879])
 # print(myVector)
 
 # myVector = Vector([1, 2, 3, 4])
 
 v = Vector([8.218, -9.341])
-w = Vector([-1.129, 2.111])
-print(myVector.dot(w))
+w = Vector([0.825, 2.036])
+print(myVector.component_orthogonal_to(w))
+print(1e-10 > 0.1)
